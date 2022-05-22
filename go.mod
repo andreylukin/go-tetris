e71631a5350a1fcd2a23cade67b38d33db5dd689
Andrey Lukin <luk.andrey@gmail.com>
2022-05-21 21:51:34 -0400

    commit with basic screen use

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/andreylukin/go-tetris
+
+go 1.18
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell v1.4.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/urfave/cli v1.22.9 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
--- main.go
+++ main.go
@@ -1,19 +1,94 @@
 package main
 
 import (
-	"io"
-	"log"
-	"net/http"
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/gdamore/tcell"
 )
 
-func main() {
-	// Hello world, the web server
+func makebox(s tcell.Screen) {
+	w, h := s.Size()
 
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
+	if w == 0 || h == 0 {
+		return
 	}
 
-	http.HandleFunc("/hello", helloHandler)
-	log.Println("Listing for requests at http://localhost:8000/hello")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	lx := rand.Int() % w
+	ly := rand.Int() % h
+	lw := rand.Int() % (w - lx)
+	lh := rand.Int() % (h - ly)
+	st := tcell.StyleDefault
+	gl := ' '
+
+	if s.Colors() > 1 {
+		// fmt.Println("hello")
+		st = st.Background(tcell.Color(rand.Int() % s.Colors()))
+	}
+
+	for row := 0; row < lh; row++ {
+		for col := 0; col < lw; col++ {
+			s.SetCell(lx+col, ly+row, st, gl)
+		}
+	}
+	s.Show()
+}
+
+func main() {
+
+	tcell.SetEncodingFallback(tcell.EncodingFallbackASCII)
+	s, e := tcell.NewScreen()
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", e)
+		os.Exit(1)
+	}
+	if e = s.Init(); e != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", e)
+		os.Exit(1)
+	}
+
+	// s.SetStyle(tcell.StyleDefault.
+	// 	Foreground(tcell.ColorBlack).
+	// 	Background(tcell.ColorWhite))
+	// s.Clear()
+
+	quit := make(chan struct{})
+	go func() {
+		for {
+			ev := s.PollEvent()
+			switch ev := ev.(type) {
+			case *tcell.EventKey:
+				switch ev.Key() {
+				case tcell.KeyEscape, tcell.KeyEnter:
+					close(quit)
+					return
+				case tcell.KeyCtrlL:
+					s.Sync()
+				}
+			case *tcell.EventResize:
+				s.Sync()
+			}
+		}
+	}()
+
+	cnt := 0
+	dur := time.Duration(0)
+loop:
+	for {
+		select {
+		case <-quit:
+			break loop
+		case <-time.After(time.Millisecond * 50):
+		}
+		start := time.Now()
+		makebox(s)
+		cnt++
+		dur += time.Now().Sub(start)
+	}
+
+	s.Fini()
+	fmt.Printf("Finished %d boxes in %s\n", cnt, dur)
+	fmt.Printf("Average is %0.3f ms / box\n", (float64(dur)/float64(cnt))/1000000.0)
 }
